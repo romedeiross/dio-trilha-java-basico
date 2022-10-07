bbf234ee9841ccea393bc68e51f59ed03b931467
Romedeiros <rosaliamedeirossilva@gmail.com>
2022-10-07 15:12:57 -0300

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,21 @@
+public class Operadores {
+    public static void main(String[] args) {
+     // Operadores de atribuição "="
+    int a = 5;
+    int b = 2;
+     
+    //Operadores Aritméticos    
+     int soma = a + b; // soma "+"
+     int subtracao = a - b; // subtração "-"
+     double multiplicacao = a * b; //multiplicação "*"    
+     double divisao = a / b ; //Divisão "/"
+     int modulo = a % b; //Resto da divisão
+     
+    System.out.println(soma);
+    System.out.println(subtracao);
+    System.out.println(multiplicacao);
+    System.out.println(divisao);  
+    System.out.println(modulo);
+    }
+    
+}
