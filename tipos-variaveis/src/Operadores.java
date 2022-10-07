a2a59dc9d60aac2b6b105d373af96d619454b2a0
Romedeiros <rosaliamedeirossilva@gmail.com>
2022-10-07 15:25:43 -0300

    Operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -10,12 +10,20 @@ public class Operadores {
      double multiplicacao = a * b; //multiplicação "*"    
      double divisao = a / b ; //Divisão "/"
      int modulo = a % b; //Resto da divisão
-     
+
+     // Operador concatenização "+" o simbolo de + quando utilizando em entre duas strings não soma as duas, ele concatena, ou seja ele junta as duas strings, a partir do momento que é detectado um caracter ele deixa de efetuar a operação de soma.
+     String nome, sobrenome;
+     nome = "Pedro";
+     sobrenome = "Souza";    
+
     System.out.println(soma);
     System.out.println(subtracao);
     System.out.println(multiplicacao);
     System.out.println(divisao);  
     System.out.println(modulo);
+
+    System.out.println(nome + " " + sobrenome);
+
     }
     
 }
