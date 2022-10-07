e00b7bfee05c22be4bea76d907b989d544e1768c
Romedeiros <rosaliamedeirossilva@gmail.com>
2022-10-07 16:24:22 -0300

    OperadoresRelacionais

---
+++ tipos-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,48 @@
+public class OperadoresRelacionais {
+/*Operadores relacionais avaliam  a relação entre duas  expressões e/ou variáveis.*/
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+    //== Verifica se as variáveis são iguais.
+        if (a==b) {
+            System.out.println("Verdadeiro");
+        }else {
+            System.out.println("Falso");
+        }
+    //!= Verifica se uma variável é diferente da outra
+        if (a!=b) {
+            System.out.println("Verdadeiro");
+        }else {
+            System.out.println("Falso");
+        }
+    //> Verifica se uma variavel é maior que a outra
+        
+        if (a > b) {
+            System.out.println("Verdadeiro");
+        }else {
+            System.out.println("Falso");
+        }
+
+    //< Verifica se uma variavel é menor que a outra
+        if (a < b) {
+            System.out.println("Verdadeiro");
+        }else {
+            System.out.println("Falso");
+        }
+
+    // >= Verifica se uma variavel é maior ou igual que a outra
+        if (a >= b) {
+            System.out.println("Verdadeiro");
+        }else {
+            System.out.println("Falso");
+        }
+
+    // <= Verifica se uma variavel é menor ou igual que a outra
+        if (a >= b) {
+            System.out.println("Verdadeiro");
+        }else {
+            System.out.println("Falso");
+        }
+    }
+}
